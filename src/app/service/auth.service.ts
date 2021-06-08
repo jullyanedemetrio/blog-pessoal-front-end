8b2e53c6a3b7d23675d38b1ba87743981363ebe7
Jullyane <jullyanedmt@gmail.com>
2021-06-08 19:34:56 -0300

    feat: implementação do crud de postagem e update de usuário

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -25,6 +25,14 @@ export class AuthService {
     return this.http.post<User>(`${environment.server}/usuarios/cadastrar`, user)
   }
 
+  atualizar(user: User): Observable<User>{
+    return this.http.put<User>(`${environment.server}/usuarios/cadastrar`, user)
+  }
+
+  getByIdUser(id: number): Observable<User>{
+    return this.http.get<User>(`${environment.server}/usuarios/${id}`)
+  }
+
   logado() {
     let ok: boolean = false
 
---
+++ src/app/service/postagem.service.ts
@@ -0,0 +1,40 @@
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostagemService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  //Objeto para colocar o token no headers
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+
+  getAllPostagens(): Observable<Postagem[]>{
+    return this.http.get<Postagem[]>(`${environment.server}/postagens`, this.token)
+  }
+
+  getByIdPostagem(id: number): Observable<Postagem>{
+    return this.http.get<Postagem>(`${environment.server}/postagens/${id}`, this.token)
+  }
+
+  postPostagem(postagem: Postagem): Observable<Postagem>{
+    return this.http.post<Postagem>(`${environment.server}/postagens`, postagem, this.token)
+  }
+
+  putPostagem(postagem: Postagem): Observable<Postagem>{
+    return this.http.put<Postagem>(`${environment.server}/postagens`, postagem, this.token)
+  }
+
+  deletePostagem(id: number){
+    return this.http.delete(`${environment.server}/postagens/${id}`, this.token)
+  }
+}
